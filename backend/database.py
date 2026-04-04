d97f9ab5516a86f602d2efd99e21503c3106a20c
Jimson-paul <jimsonpaul99@gmail.com>
2026-04-04 22:23:29 +0530

    Added connection ping and environment variables

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # Just remember to replace YOUR_GENERATED_PASSWORD with the password you generated earlier!
 DATABASE_URL = "mysql+pymysql://2K3gYUe4LSEWSLX.root:rOPczS0IsBGtYz4I@gateway01.ap-northeast-1.prod.aws.tidbcloud.com:4000/test?ssl_verify_cert=true&ssl_verify_identity=true"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+    pool_pre_ping=True,  # Checks if the database is still listening
+    pool_recycle=300     # Refreshes the connection every 5 minutes)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
