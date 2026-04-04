6d62ee55d70e9de20975b5c58d1d9f196f657130
Jimson-paul <jimsonpaul99@gmail.com>
2026-04-04 22:43:50 +0530

    Fixed HTTP to HTTPS to prevent Android Cleartext block on Report Accident screen

--- lib/report_accident_screen.dart
+++ lib/report_accident_screen.dart
@@ -135,7 +135,7 @@ class _ReportAccidentScreenState extends State<ReportAccidentScreen> {
 
     final request = http.MultipartRequest(
       "POST",
-      Uri.parse("http://10.0.2.2:8000/report-accident"),
+      Uri.parse("https://safehorizon-3.onrender.com/report-accident"),
     );
 
     request.fields["user_email"] = widget.userEmail;
