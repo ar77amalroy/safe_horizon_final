0700fac45a23738e4ce23a7b3fb29540a54fe059
Amal Roy <amalroy77ar77@gmail.com>
2026-04-06 23:51:53 +0530

    Add fallback defaults to email_config.py

--- backend/email_config.py
+++ backend/email_config.py
@@ -2,9 +2,9 @@ from fastapi_mail import ConnectionConfig
 import os
 
 conf = ConnectionConfig(
-    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME", "safehorizonalerts@gmail.com"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD", "dauo htxh ajfd bsxn"),
+    MAIL_FROM=os.getenv("MAIL_FROM", "safehorizonalerts@gmail.com"),
 
     MAIL_SERVER=os.getenv("MAIL_SERVER", "smtp.gmail.com"),
     MAIL_PORT=int(os.getenv("MAIL_PORT", 465)),
