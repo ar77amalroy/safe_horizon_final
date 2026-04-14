2ebee771fce961587ab5b7294ac70e123fdcff84
Amal Roy <amalroy77ar77@gmail.com>
2026-04-14 11:28:40 +0530

    Update config IP to hotspot address

--- lib/config.dart
+++ lib/config.dart
@@ -1,12 +1,12 @@
 class AppConfig {
   // ⚠️ INSTRUCTIONS FOR CONNECTING YOUR DEVICE:
   // 1. Find your computer's local IP address (e.g., run 'ipconfig' on Windows).
-  // 2. Replace '192.168.1.4' with your actual IP address.
+  // 2. Replace '10.197.96.244' with your actual IP address.
   // 3. Ensure your phone and computer are on the SAME Wi-Fi network.
 
   // 📱 PHYSICAL PHONE URL (Local Network)
   // Replace the IP below with your computer's IP address!
-  static const String baseUrl = "http://192.168.1.4:8000";
+  static const String baseUrl = "http://10.197.96.244:8000";
 
   // ✅ ANDROID EMULATOR URL
   // static const String baseUrl = "http://10.0.2.2:8000";
